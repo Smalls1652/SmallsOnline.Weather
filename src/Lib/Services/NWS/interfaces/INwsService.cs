e3b82fbef72759ea3d332f41aa83b09f264a1bbf
Timothy Small <smalls.online@icloud.com>
2023-05-22 19:24:04 -0400

    Add methods to interface

--- src/Lib/Services/NWS/interfaces/INwsService.cs
+++ src/Lib/Services/NWS/interfaces/INwsService.cs
@@ -1,6 +1,16 @@
-﻿namespace SmallsOnline.Weather.Lib.Services.NWS;
+﻿using SmallsOnline.Weather.Lib.Models.NWS;
+
+namespace SmallsOnline.Weather.Lib.Services.NWS;
 
 public interface INwsService
 {
-    
+    // API endpoints: /offices
+    Task<Office?> GetOfficeAsync(string officeId);
+
+    // API endpoints: /points
+    Task<GeoJsonFeature<Point>?> GetPointInfoAsync(string latitude, string longitude);
+
+    // API endpoints: /products
+    Task<GraphCollection<TextProduct>?> GetProductEntriesAsync(string product, string officeId);
+    Task<TextProduct?> GetProductEntryAsync(string productId);
 }
