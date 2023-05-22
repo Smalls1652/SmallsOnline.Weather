39a3babc99f546a586c0ae89423045532b8856db
Timothy Small <smalls.online@icloud.com>
2023-05-22 16:10:08 -0400

    Add JsonSourceGenerationContext

---
+++ src/Lib/JsonSourceGenerationContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using SmallsOnline.Weather.Lib.Models.NWS;
+
+namespace SmallsOnline.Weather.Lib;
+
+[JsonSourceGenerationOptions(
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    GenerationMode = JsonSourceGenerationMode.Default,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+)]
+[JsonSerializable(typeof(Alert))]
+[JsonSerializable(typeof(AlertCollectionGeoJson))]
+[JsonSerializable(typeof(GeoJsonGeometry))]
+[JsonSerializable(typeof(GeoJsonFeature<Alert>))]
+[JsonSerializable(typeof(Office))]
+[JsonSerializable(typeof(OfficeAddress))]
+[JsonSerializable(typeof(OfficeHeadline))]
+[JsonSerializable(typeof(OfficeHeadlines))]
+[JsonSerializable(typeof(PaginationInfo))]
+[JsonSerializable(typeof(Point))]
+[JsonSerializable(typeof(ProblemDetail))]
+[JsonSerializable(typeof(TextProduct))]
+internal partial class JsonSourceGenerationContext : JsonSerializerContext
+{
+}
