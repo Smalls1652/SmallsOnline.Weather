b8160b76a8587961c9c1b39028797fb17e1eaca8
Timothy Small <smalls.online@icloud.com>
2023-05-22 19:23:02 -0400

    Add GetOfficeAsync()

---
+++ src/Lib/Services/NWS/Offices/GetOfficeAsync.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using SmallsOnline.Weather.Lib.Models.NWS;
+
+namespace SmallsOnline.Weather.Lib.Services.NWS;
+
+public partial class NwsService
+{
+    public async Task<Office?> GetOfficeAsync(string officeId)
+    {
+        HttpRequestMessage request = new(
+            method: HttpMethod.Get,
+            requestUri: $"https://api.weather.gov/offices/{officeId}"
+        );
+
+        HttpResponseMessage apiResponse = await _httpClient.SendAsync(request);
+
+        string jsonString = await apiResponse.Content.ReadAsStringAsync();
+
+        if (!apiResponse.IsSuccessStatusCode)
+        {
+            ProblemDetail? problemDetail = ParseForProblemDetail(jsonString);
+            throw new NwsApiException($"An error occured getting forecast office data: {problemDetail!.Detail}", problemDetail);
+        }
+
+        return JsonSerializer.Deserialize(
+            json: jsonString,
+            jsonTypeInfo: _sourceGenerationContext.Office
+        );
+    }
+}
