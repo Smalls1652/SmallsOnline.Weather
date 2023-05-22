de4838dd2c7d4323b88524c9c625bac91bdbe772
Timothy Small <smalls.online@icloud.com>
2023-05-22 19:18:04 -0400

    Add custom NWS API exception

---
+++ src/Lib/Models/NWS/NwsApiException.cs
@@ -0,0 +1,19 @@
+﻿namespace SmallsOnline.Weather.Lib.Models.NWS;
+
+public class NwsApiException : Exception
+{
+    public NwsApiException()
+    {}
+
+    public NwsApiException(string message, ProblemDetail problemDetail) : base(message)
+    {
+        NwsApiProblemDetail = problemDetail;
+    }
+
+    public NwsApiException(string message, ProblemDetail problemDetail, Exception innerException) : base(message, innerException)
+    {
+        NwsApiProblemDetail = problemDetail;
+    }
+
+    public ProblemDetail? NwsApiProblemDetail { get; }
+}
