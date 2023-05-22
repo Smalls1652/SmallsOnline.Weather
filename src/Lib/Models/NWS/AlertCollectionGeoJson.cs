a22f2e0fd4b93a2edf6dd13428e02ad9098297bd
Timothy Small <smalls.online@icloud.com>
2023-05-22 16:05:48 -0400

    Add AlertCollectionGeoJson

---
+++ src/Lib/Models/NWS/AlertCollectionGeoJson.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmallsOnline.Weather.Lib.Models.NWS;
+
+public class AlertCollectionGeoJson : IAlertCollectionGeoJson
+{
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("updated")]
+    public DateTimeOffset LastUpdated { get; set; }
+
+    [JsonPropertyName("features")]
+    public IEnumerable<GeoJsonFeature<Alert>>? Alerts { get; set; }
+
+    [JsonPropertyName("pagination")]
+    public PaginationInfo? Pagination { get; set; }
+}
---
+++ src/Lib/Models/NWS/interfaces/IAlertCollectionGeoJson.cs
@@ -0,0 +1,10 @@
+﻿namespace SmallsOnline.Weather.Lib.Models.NWS;
+
+public interface IAlertCollectionGeoJson
+{
+    string Type { get; set; }
+    string Title { get; set; }
+    DateTimeOffset LastUpdated { get; set; }
+    IEnumerable<GeoJsonFeature<Alert>>? Alerts { get; set; }
+    PaginationInfo? Pagination { get; set; }
+}
