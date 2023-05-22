f938e00e19d26053e3a8cf5ae2a02747d1691f5b
Timothy Small <smalls.online@icloud.com>
2023-05-22 16:05:33 -0400

    Add GeoJsonFeature

---
+++ src/Lib/Models/NWS/GeoJsonFeature.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmallsOnline.Weather.Lib.Models.NWS;
+
+public class GeoJsonFeature<T> : IGeoJsonFeature<T>
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = null!;
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = null!;
+
+    [JsonPropertyName("geometry")]
+    public GeoJsonGeometry Geometry { get; set; } = null!;
+
+    [JsonPropertyName("properties")]
+    public IEnumerable<T>? Properties { get; set; }
+}
---
+++ src/Lib/Models/NWS/interfaces/IGeoJsonFeature.cs
@@ -0,0 +1,9 @@
+﻿namespace SmallsOnline.Weather.Lib.Models.NWS;
+
+public interface IGeoJsonFeature<T>
+{
+    string Id { get; set; }
+    string Type { get; set; }
+    GeoJsonGeometry Geometry { get; set; }
+    IEnumerable<T>? Properties { get; set; }
+}
