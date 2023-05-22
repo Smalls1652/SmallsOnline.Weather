4473bdece229739e072e788d2d30cf532ed5637e
Timothy Small <smalls.online@icloud.com>
2023-05-22 19:23:38 -0400

    Add GetProductEntriesAsync()

---
+++ src/Lib/Services/NWS/Products/GetProductEntriesAsync.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using SmallsOnline.Weather.Lib.Models.NWS;
+
+namespace SmallsOnline.Weather.Lib.Services.NWS;
+
+public partial class NwsService
+{
+    public async Task<GraphCollection<TextProduct>?> GetProductEntriesAsync(string product, string officeId)
+    {
+        HttpRequestMessage request = new(
+            method: HttpMethod.Get,
+            requestUri: $"https://api.weather.gov/products/types/{product}/locations/{officeId}"
+        );
+
+        HttpResponseMessage apiResponse = await _httpClient.SendAsync(request);
+
+        string jsonString = await apiResponse.Content.ReadAsStringAsync();
+
+        if (!apiResponse.IsSuccessStatusCode)
+        {
+            ProblemDetail? problemDetail = ParseForProblemDetail(jsonString);
+            throw new NwsApiException($"An error occured getting product info: {problemDetail!.Detail}", problemDetail);
+        }
+
+        return JsonSerializer.Deserialize(
+            json: jsonString,
+            jsonTypeInfo: _sourceGenerationContext.GraphCollectionTextProduct
+        );
+    }
+}
