b78b46b1152d1bbda454fc709e82a295f4c4864a
Timothy Small <smalls.online@icloud.com>
2023-05-25 17:38:54 -0400

    Add NwsService during startup

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SmallsOnline.Weather.Lib.Services.NWS;
 using SmallsOnline.Weather.WebApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddLogging();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<INwsService, NwsService>();
 
 await builder.Build().RunAsync();
